c1b83b9707d2aefa4b38696f9b32f46b01ceec3b
Muaaz1992 <Muaaz1992@yahoo.com>
2023-02-18 08:34:32 +0500

    Installed RReact Router adn React Router DOm and Created Pages and Routes

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import ForgotPassword from "./pages/ForgotPassword.jsx";
+import Offers from "./pages/Offers.jsx";
+import Profiles from "./pages/Profiles.jsx";
+import SignIn from "./pages/SignIn.jsx";
+import SignUp from "./pages/SignUp.jsx";
+
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profiles" element={<Profiles />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
