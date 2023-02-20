eacd99a62f690004961eae6be06fa483b27c0617
Muaaz1992 <Muaaz1992@yahoo.com>
2023-02-20 18:47:20 +0500

    Made the Spinner and styled it to stay dead center of the page and added transparenct effect

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -27,7 +27,7 @@ export default function Header() {
      }
 
   return (
-    <div className="bg-white border-b shadow-sm sticky top-0 z-50" >
+    <div className="bg-white border-b shadow-sm sticky top-0 z-40" >
         <header className="flex items-center justify-between max-w-6xl mx-auto px-3" >
             <div>
                 <img src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg" alt="logo " className="cursor-pointer h-5" onClick={()=>navigate("/")} />
--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,10 +1,11 @@
 import { Outlet, Navigate} from 'react-router-dom';
 import {useAuthStatus} from '../Hooks/useAuthStatus';
+import Spinner from './Spinner';
 
 export default function PrivateRoute() {
     const{loggedIn, checkingStatus} = useAuthStatus();
     if(checkingStatus){
-        return<h3>Loading...</h3>
+        return <h3><Spinner /></h3>
     }
     return loggedIn ? <Outlet /> : <Navigate to="/sign-in"/>
 
