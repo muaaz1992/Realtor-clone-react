8a8d6b461214137d52b83b42b5eb07269e6234cc
Muaaz1992 <Muaaz1992@yahoo.com>
2023-03-10 12:15:51 +0500

    Added Properties for Rent and Properties for sale query in Homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,6 @@ import { db } from "../firebase";
 export default function Home() {
   // Offers
   const [offerListings, setOfferListings] = useState(null);
-
   useEffect(() => {
     async function fetchListings() {
       try {
@@ -42,6 +41,62 @@ export default function Home() {
     }
     fetchListings();
   }, []);
+  // Places for Rent
+  const [rentListings, setRentListings] = useState(null);
+  useEffect(() => {
+    async function fetchListings() {
+      try {
+        const listingsRef = collection(db, "listings");
+        const q = query(
+          listingsRef,
+          where("type", "==", "rent"),
+          orderBy("timeStamp", "desc"),
+          limit(4)
+        );
+
+        const querySnap = await getDocs(q);
+        const listings = [];
+        querySnap.forEach((doc) => {
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+        setRentListings(listings);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchListings();
+  }, []);
+  // Places for Sale
+  const [saleListings, setSaleListings] = useState(null);
+  useEffect(() => {
+    async function fetchListings() {
+      try {
+        const listingsRef = collection(db, "listings");
+        const q = query(
+          listingsRef,
+          where("type", "==", "sale"),
+          orderBy("timeStamp", "desc"),
+          limit(4)
+        );
+
+        const querySnap = await getDocs(q);
+        const listings = [];
+        querySnap.forEach((doc) => {
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+        setSaleListings(listings);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchListings();
+  }, []);
 
   return (
     <div>
@@ -55,8 +110,50 @@ export default function Home() {
                 Show more offers
               </p>
             </Link>
-            <ul className="lg:grid-cols-3 mt-6 sm:grid sm:grid-cols-2 xl:grid-cols-4">
+            <ul className="lg:grid-cols-3 mt-3 sm:grid sm:grid-cols-2 xl:grid-cols-4">
               {offerListings.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  listing={listing.data}
+                  id={listing.id}
+                />
+              ))}
+            </ul>
+          </div>
+        )}
+        {rentListings && rentListings.length > 0 && (
+          <div className="m-2 mb-6">
+            <h2 className="font-semibold mt-6 px-3 text-2xl">
+              Places for Rent
+            </h2>
+            <Link to={"/category/rent"}>
+              <p className="duration-150 ease-in-out hover:text-blue-800 px-3 text-blue-600 text-sm transition">
+                Show more places for rent
+              </p>
+            </Link>
+            <ul className="lg:grid-cols-3 mt-3 sm:grid sm:grid-cols-2 xl:grid-cols-4">
+              {rentListings.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  listing={listing.data}
+                  id={listing.id}
+                />
+              ))}
+            </ul>
+          </div>
+        )}
+        {saleListings && saleListings.length > 0 && (
+          <div className="m-2 mb-6">
+            <h2 className="font-semibold mt-6 px-3 text-2xl">
+              Places for Sale
+            </h2>
+            <Link to={"/category/sale"}>
+              <p className="duration-150 ease-in-out hover:text-blue-800 px-3 text-blue-600 text-sm transition">
+                Show more places for sale
+              </p>
+            </Link>
+            <ul className="lg:grid-cols-3 mt-3 sm:grid sm:grid-cols-2 xl:grid-cols-4">
+              {saleListings.map((listing) => (
                 <ListingItem
                   key={listing.id}
                   listing={listing.data}
